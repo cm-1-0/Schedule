c63094c41cbe4bfa8ba52c87a0c8a1a78c6ad3d5
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2021-05-09 22:51:03 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+schedule = {
+    "Monday": m1
+}
+q1 = input("Enter Schedule or Get Schedule: ")
+if q1 == "Enter Schedule":
+    q2 = input("Pick a Day: ")
+    if q2 == "Monday":
+        m1 = input("Enter your Plans: ")
+
+
