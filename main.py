0f3c8b70367334acd6c5fb61c1e126e4339c2d5f
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2021-05-10 17:49:46 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-schedule = {
-    "Monday": m1
+m1 = "Plans for Monday"
+t1 = "Plans for Tuesday"
+w1 = "Plans for Wednesday"
+th1 = "Plans for Thursday"
+f1 = "Plans for Friday"
+s1 = "Plans for Saturday"
+su1 = "Plans for Sunday"
+day = input("Pick a Day: ")
+if day == "Monday":
+    print(m1)
+if day == "Tuesday":
+    print(t1)
+if day == "Wednesday":
+    print(w1)
+if day == "Thursday":
+    print(th1)
+if day == "Friday":
+    print(f1)
+if day == "Saturday":
+    print(s1)
+if day == "Sunday":
+    print(su1)
+
+enter_schedule = {
+    "Monday": m1,
+    "Tuesday": t1,
+    "Wednesday": w1,
+    "Thursday": th1,
+    "Friday": f1,
+    "Saturday": s1,
+    "Sunday": su1
 }
-q1 = input("Enter Schedule or Get Schedule: ")
-if q1 == "Enter Schedule":
-    q2 = input("Pick a Day: ")
-    if q2 == "Monday":
-        m1 = input("Enter your Plans: ")
-
-
